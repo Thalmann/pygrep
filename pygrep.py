c2e422a780f874b7e575a3e9e83b450f0e53d896
Thalmann <mr.thalmann@gmail.com>
2016-02-11 22:34:51 +0100

    Added recursive search argument and added yield.

--- pygrep.py
+++ pygrep.py
@@ -7,22 +7,26 @@ dirs = list()
 hits = list()
 
 parser = argparse.ArgumentParser()
-parser.add_argument('search_term')
+parser.add_argument('search_term', help='insert the search term you want to search for')
+parser.add_argument('-r', help='search recursively', action='store_true')
 args = parser.parse_args()
 search_term = args.search_term
+recursive_search = args.r
 
+# Should use yield return
 def get_files_recursive(path):
-    files = list()
     for dir_path, dir_names, file_names in os.walk(path):
         for file_name in file_names:
-            files.append(os.path.join(dir_path,file_name))
+            yield os.path.join(dir_path,file_name)
         for dir_name in dir_names:
             dirs.append(os.path.join(dir_path, dir_name))
-    return files
             
 def get_files(path):
-    return os.listdir(path)
+    for f in os.listdir(path):
+        if os.path.isfile(os.path.join(path, f)):
+            yield f
 
+            
 def search_file(file_path, search_term):
     with open(file_path, 'r') as f:
         try:
@@ -39,9 +43,12 @@ def highlight_search_term(line, search_term):
     return line.replace(search_term,colored(search_term, 'red'))
 
 
-
-for f in get_files_recursive('.'):
-    search_file(f, search_term)
+if recursive_search:
+    for f in get_files_recursive('.'):
+        search_file(f, search_term)
+else:
+    for f in get_files('.'):
+        search_file(f, search_term)
 
 file_number = input('Insert file number:\n')
 print(hits[int(file_number)-1])
