d22fd88255bbf1e7732ec703496b4d3026506a5c
Thalmann <mr.thalmann@gmail.com>
2016-02-11 11:24:45 +0100

    Added search term argument and renamed keyword.

--- pygrep.py
+++ pygrep.py
@@ -1,10 +1,16 @@
 import os
 from termcolor import colored
+import argparse
 
 cwd = os.getcwd()
 files = list()
 dirs = list()
 hits = list()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('search_term')
+args = parser.parse_args()
+search_term = args.search_term
 
 def get_files(path, recursive=True):
     for dir_path, dir_names, file_names in os.walk(path):
@@ -13,20 +19,20 @@ def get_files(path, recursive=True):
         for dir_name in dir_names:
             dirs.append(os.path.join(dir_path, dir_name))
         
-def search_file(file_path, keyword):
+def search_file(file_path, search_term):
     with open(file_path, 'r') as f:
         try:
             line_number = 0
             for line in f:
                 line_number += 1
-                if keyword in line:
+                if search_term in line:
                     hits.append(file_path)
-                    print(str(len(hits)) + colored(': ', 'blue') + file_path + colored(': ', 'blue') + str(line_number)+ ':' + highlight_keyword(line, keyword))
+                    print(str(len(hits)) + colored(': ', 'blue') + file_path + colored(': ', 'blue') + str(line_number)+ ':' + highlight_search_term(line, search_term))
         except UnicodeDecodeError:
             return
 
-def highlight_keyword(line, keyword):
-    return line.replace(keyword,colored(keyword, 'red'))
+def highlight_search_term(line, search_term):
+    return line.replace(search_term,colored(search_term, 'red'))
 
 get_files('.')
 
@@ -34,7 +40,7 @@ get_files('.')
 #print(dirs)
 
 for f in files:
-    search_file(f, 'ECB')
+    search_file(f, search_term)
 
 file_number = input('Insert file number:\n')
 print(hits[int(file_number)-1])
